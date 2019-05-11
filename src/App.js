735d54f5692e71eb492f18ca27c4c3e05b958f8f
Gabriel Martinez <gibbi33@gmail.com>
2019-05-11 13:03:29 -0700

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,370 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Wizard, Steps, Step } from 'react-albus';
+import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import AppBar from '@material-ui/core/AppBar';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Chip from '@material-ui/core/Chip';
 
-function App() {
+const styles = theme => ({
+  appBar: {
+    position: 'relative',
+  },
+  icon: {
+    marginRight: theme.spacing.unit * 2,
+  },
+  heroUnit: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  heroContent: {
+    maxWidth: 600,
+    margin: '0 auto',
+    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
+  },
+  heroButtons: {
+    marginTop: theme.spacing.unit * 4,
+  },
+  mainUnit: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  mainContent: {
+    maxWidth: 600,
+    margin: '0 auto',
+    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
+  },
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+      width: 1100,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  cardGrid: {
+    padding: `${theme.spacing.unit * 8}px 0`,
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing.unit * 6,
+  },
+});
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+function App(props) {
+  const { classes } = props;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <CameraIcon className={classes.icon} />
+          <Typography variant="h6" color="inherit" noWrap>
+            ASU Venture Ecosystem
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <main>
+
+        <div className={classes.heroUnit}>
+          <div className={classes.heroContent}>
+
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              ASU Venture Ecosystem
+            </Typography>
+            <Typography variant="h6" align="center" color="textSecondary" gutterBottom>
+              Description of project goes here.
+            </Typography>
+
+            <div className={classes.heroButtons}>
+              <Grid container spacing={16} justify="center">
+
+              </Grid>
+            </div>
+          </div>
+        </div>
+
+        <div className={classes.mainUnit}>
+          <div className={classes.mainContent}>
+
+        <Wizard>
+          <Steps>
+
+            <Step
+              id="first"
+              render={({ next }) => (
+                <React.Fragment>
+
+                  <Typography variant="h4" gutterBottom>
+                    To see available funding tracks, first tell us about yourself ...
+                  </Typography>
+
+                  <Typography variant="h6" gutterBottom>
+                    I am:<br />(Select all that apply)
+                  </Typography>
+
+                  <Chip
+                    label="Affiliated with ASU"
+                    className={classes.chip}
+                    component="a"
+                    href="#"
+                    clickable
+                  />
+
+                  <Chip
+                    label="An Undergrad Student"
+                    className={classes.chip}
+                    component="a"
+                    href="#"
+                    clickable
+                  />
+
+                  <Chip
+                    label="A Graduate Student"
+                    className={classes.chip}
+                    component="a"
+                    href="#"
+                    clickable
+                  />
+
+                  <Chip
+                    label="Faculty"
+                    className={classes.chip}
+                    component="a"
+                    href="#"
+                    clickable
+                  />
+
+                  <Chip
+                    label="University Staff"
+                    className={classes.chip}
+                    component="a"
+                    href="#"
+                    clickable
+                  />
+
+                  <Chip
+                    label="An Owner of a Company"
+                    className={classes.chip}
+                    component="a"
+                    href="#"
+                    clickable
+                  />
+
+                  <Typography variant="h6" gutterBottom>
+                    Congratulations on owning a company.<br />What is its current valuation?
+                  </Typography>
+
+                  <Chip
+                    label="$0-500K"
+                    className={classes.chip}
+                    component="a"
+                    href="#"
+                    clickable
+                  />
+
+                  <Chip
+                    label="$500K-1mil"
+                    className={classes.chip}
+                    component="a"
+                    href="#"
+                    clickable
+                  />
+
+                  <Chip
+                    label="$1-3mil"
+                    className={classes.chip}
+                    component="a"
+                    href="#"
+                    clickable
+                  />
+
+                  <Chip
+                    label="$3-15mil"
+                    className={classes.chip}
+                    component="a"
+                    href="#"
+                    clickable
+                  />
+
+                  <Chip
+                    label="$15-75mil"
+                    className={classes.chip}
+                    component="a"
+                    href="#"
+                    clickable
+                  />
+
+                  <Chip
+                    label="$75-500mil"
+                    className={classes.chip}
+                    component="a"
+                    href="#"
+                    clickable
+                  />
+
+                  <Grid item>
+                    <Button variant="contained" color="primary" onClick={next}>
+                      Next
+                    </Button>
+                  </Grid>
+
+                </React.Fragment>
+              )}
+            />
+
+            <Step
+              id="second"
+              render={({ next, previous }) => (
+                <React.Fragment>
+
+                  <p>Summary from first section goes here</p>
+
+                  <h2>I'm interested in funding to support:<br />(Select one)</h2>
+                  <ul>
+                    <li>Ideation to Prototyping</li>
+                    <li>Ideation to Business Formation</li>
+                    <li>Prototype to Business Formation</li>
+                    <li>Prototype to Customer Acquisition</li>
+                    <li>Customer Acquisition &amp; Emerging Growth</li>
+                  </ul>
+
+                  <h2>... in the areas of:<br />(Select all that apply)</h2>
+                  <ul>
+                    <li>Manufacturing/ Hardware</li>
+                    <li>Social/ Sports</li>
+                    <li>Health</li>
+                    <li>Veterans</li>
+                    <li>Media</li>
+                    <li>General Technology</li>
+                    <li>EdTech</li>
+                    <li>Internet of Things</li>
+                    <li>Other</li>
+                  </ul>
+
+                  <Grid item>
+                    <Button variant="contained" color="primary" onClick={previous}>
+                      Previous
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button variant="outlined" color="primary" onClick={next}>
+                      Next
+                    </Button>
+                  </Grid>
+
+                </React.Fragment>
+              )}
+            />
+
+            <Step
+              id="summary"
+              render={({ previous }) => (
+                <React.Fragment>
+
+                  <p>summary of and edit first section | option to edit first section</p>
+                  <p>summary of and edit second section | option to edit second section</p>
+                  <p>print option</p>
+                  <p>email input and send option</p>
+                  <h2>Here are all the ways the ASU Venture Ecosystem can help:</h2>
+                  <p>results go here</p>
+
+                  <div className={classNames(classes.layout, classes.cardGrid)}>
+
+                    <Grid container spacing={40}>
+                      {cards.map(card => (
+                        <Grid item key={card} sm={6} md={4} lg={3}>
+                          <Card className={classes.card}>
+                            <CardMedia
+                              className={classes.cardMedia}
+                              image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
+                              title="Image title"
+                            />
+                            <CardContent className={classes.cardContent}>
+                              <Typography gutterBottom variant="h5" component="h2">
+                                Fund Title
+                              </Typography>
+                              <Typography>
+                                Fund details.
+                              </Typography>
+                            </CardContent>
+                            <CardActions>
+                              <Button size="small" color="primary">
+                                More info ...
+                              </Button>
+                            </CardActions>
+                          </Card>
+                        </Grid>
+                      ))}
+                    </Grid>
+
+                  </div>
+
+                  <Grid item>
+                    <Button variant="contained" color="primary" onClick={previous}>
+                      Previous
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button variant="outlined" color="secondary">
+                      Start Over
+                    </Button>
+                  </Grid>
+
+                </React.Fragment>
+              )}
+            />
+
+          </Steps>
+        </Wizard>
+
+        </div>
+        </div>
+
+      </main>
+
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          ASU Venture Ecosystem
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Description of project goes here.
+        </Typography>
+      </footer>
+
+    </React.Fragment>
   );
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
