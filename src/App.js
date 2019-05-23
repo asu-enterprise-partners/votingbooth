e32e72a8a24a4e24dc1b6efa13dbaed1b1707fea
Gabriel Martinez <gibbi33@gmail.com>
2019-05-22 19:36:59 -0700

    First commit to gabe branch.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import './App.css';
 import { Wizard, Steps, Step } from 'react-albus';
-import Button from '@material-ui/core/Button';
+
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+
+import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
-
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControl from '@material-ui/core/FormControl';
-
 
 const styles = theme => ({
   appBar: {
