d077c93d3e9e829ca943fbf52cff5e80582450e4
Gabriel Martinez <gibbi33@gmail.com>
2019-05-29 18:59:54 -0700

    Added json-loader package and will start to parse json.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
@@ -39,11 +38,11 @@ const styles = theme => ({
     width: 'auto',
     marginLeft: theme.spacing.unit * 3,
     marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
-      width: 1100,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
+    // [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+    //   width: 1100,
+    //   marginLeft: 'auto',
+    //   marginRight: 'auto',
+    // },
   },
   cardGrid: {
     padding: `${theme.spacing.unit * 8}px 0`,
@@ -62,7 +61,8 @@ const styles = theme => ({
   checked: {},
 });
 
-const cards = [1, 2];
+const ourData = require('./Venture_Ecosystem_Data.json');
+console.log(ourData);
 
 class App extends React.Component {
 
@@ -86,10 +86,30 @@ class App extends React.Component {
     edtech: false,
     internetofthings: false,
     other: false,
+
+    results: ourData,
   };
 
   handleChangeAffiliation = name => event => {
     this.setState({ [name]: event.target.checked });
+    console.log(name);
+    // if (this.state.affiliatedwithasu === true) {
+    //   const parsedData = ourData.filter(fund => fund.Founders === 'ASU-Affiliated');
+    //   console.log(parsedData);
+    //   this.setState({
+    //     results: parsedData
+    //   })
+    // } else if (name === 'anundergradstudent') {
+    //   const parsedData = ourData.filter(fund => fund.Founders === 'Available to Anyone');
+    //   console.log(parsedData);
+    //   this.setState({
+    //     results: parsedData
+    //   })
+    // } else {
+    //   this.setState({
+    //     results: ourData
+    //   })
+    // }
   };
 
   handleChangeRadioCompanyValuation = event => {
@@ -102,6 +122,14 @@ class App extends React.Component {
 
   handleChangeAreas = name => event => {
     this.setState({ [name]: event.target.checked });
+  };
+
+  componentDidMount() {
+    console.log('can do something when main component is mounted here');
+  }
+
+  handleBlakkow () {
+    console.log('yoooo blakkow');
   };
 
   render() {
@@ -128,6 +156,8 @@ class App extends React.Component {
     const idEdTech = this.state.edtech;
     const isInternetOfThings = this.state.internetofthings;
     const isOther = this.state.other;
+
+    const ourResults = this.state.results;
 
   return (
 
@@ -490,7 +520,7 @@ class App extends React.Component {
                       Previous
                     </Button>
                     <Button variant="contained" color="primary" onClick={next}>
-                      Next
+                      Next parse results
                     </Button>
                   </Grid>
 
@@ -554,31 +584,28 @@ class App extends React.Component {
 
                   <p>print option</p>
                   <p>email input and send option</p>
-                  <h2>Here are all the ways the ASU Venture Ecosystem can help:</h2>
-                  <p>results go here</p>
+
+                  <Typography variant="h6" gutterBottom>
+                    Here are all the ways the ASU Venture Ecosystem can help:
+                  </Typography>
 
                   <div className={classNames(classes.layout, classes.cardGrid)}>
 
-                    <Grid container spacing={40}>
-                      {cards.map(card => (
-                        <Grid item key={card} sm={6} md={4} lg={3}>
+                    <Grid container spacing={32}>
+                      {ourResults.map((fund, index) => (
+                        <Grid item key={index} sm={6} md={4} lg={3}>
                           <Card className={classes.card}>
-                            <CardMedia
-                              className={classes.cardMedia}
-                              image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
-                              title="Image title"
-                            />
                             <CardContent className={classes.cardContent}>
                               <Typography gutterBottom variant="h5" component="h2">
-                                Fund Title
+                                {fund['Venture Ecosystem']}
                               </Typography>
                               <Typography>
-                                Fund details.
+                                {fund.Description}
                               </Typography>
                             </CardContent>
                             <CardActions>
-                              <Button size="small" color="primary">
-                                More info ...
+                              <Button size="small" color="primary" href={fund.URL} target="_blank">
+                                More info
                               </Button>
                             </CardActions>
                           </Card>
