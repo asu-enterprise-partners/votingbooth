d3de7c02ef6fd8237cd4f8a7243ad5e98ac85936
Cecilia Nguyen <clnguye8@m.asufoundation.org>
2020-01-30 16:36:21 -0700

    Changes were made, packages were added.
    Styled the buttons and included the dialog function.

--- src/components/MainState.js
+++ src/components/MainState.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { styled } from '@material-ui/styles';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -7,17 +8,35 @@ import MuiDialogContent from '@material-ui/core/DialogContent';
 import MuiDialogActions from '@material-ui/core/DialogActions';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
+import NatureIcon from '@material-ui/icons/Nature';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
   root: {
     margin: 0,
-    padding:'50px',
+    padding:'25px',
   },
   closeButton: {
     position: 'absolute',
+    right: '25px',
+    top: '25px',
     color: theme.palette.grey[500],
   },
+});
+
+const ContentButton = styled(Button)({
+  background: '#F1F1F1',
+  border: 'solid 10px #ffffff',
+  borderRadius: '20px',
+  height: '200px',
+  width: '200px',
+  padding: '0 30px',
+  "&:hover": {
+    backgroundColor: "#ACACAC"
+  },
+  fontSize: '110%',
 });
 
 const DialogTitle = withStyles(styles)(props => {
@@ -36,20 +55,19 @@ const DialogTitle = withStyles(styles)(props => {
 
 const DialogContent = withStyles(theme => ({
   root: {
-    padding:'50px',
+    padding:'25px',
   },
 }))(MuiDialogContent);
 
 const DialogActions = withStyles(theme => ({
   root: {
     margin: 0,
-    padding:'50px',
+    padding:'25px',
   },
 }))(MuiDialogActions);
 
 export default function CustomizedDialogs() {
   const [open, setOpen] = React.useState(false);
-
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -58,10 +76,53 @@ export default function CustomizedDialogs() {
   };
 
   return (
-    <div>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Open dialog
-      </Button>
+    <div >
+    <Grid container spacing ={24} justify = "center" >
+      <Grid item xs={4} align = "center" >
+        <ContentButton onClick={handleClickOpen}  >
+        <Typography variant="h6" gutterBottom  >
+          Environment
+        </Typography>
+        </ContentButton>
+      </Grid>
+      <Grid item xs={4} align = "center">
+        <ContentButton onClick={handleClickOpen}>
+        <Typography variant="h6" gutterBottom  >
+          Arts & Culture
+        </Typography>
+        </ContentButton>
+      </Grid>
+      <Grid item xs={4} align = "center">
+        <ContentButton onClick={handleClickOpen}>
+        <Typography variant="h6" gutterBottom  >
+          Health
+        </Typography>
+        </ContentButton>
+      </Grid>
+    </Grid>
+    <Grid container spacing ={24} justify = "center">
+      <Grid item xs={4} align = "center">
+        <ContentButton onClick={handleClickOpen}>
+        <Typography variant="h6" gutterBottom  >
+          Education
+        </Typography>
+        </ContentButton>
+      </Grid>
+      <Grid item xs={4} align = "center">
+        <ContentButton onClick={handleClickOpen}>
+        <Typography variant="h6" gutterBottom  >
+          Colleges & Programs
+        </Typography>
+        </ContentButton>
+      </Grid>
+      <Grid item xs={4} align = "center">
+        <ContentButton onClick={handleClickOpen}>
+        <Typography variant="h6" gutterBottom  >
+          Other
+        </Typography>
+        </ContentButton>
+      </Grid>
+    </Grid>
       <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
         <DialogTitle id="customized-dialog-title" onClose={handleClose}>
           Modal title
