5898581cb4cc5b4becf3a5ce2d7a6f609f7c30b9
Cecilia Nguyen <clnguye8@m.asufoundation.org>
2020-02-20 16:48:34 -0700

    Updated the results.js file!

--- src/components/Results.js
+++ src/components/Results.js
@@ -63,8 +63,30 @@ export default class Results extends Component {
               options: {
                   responsive: true,
                   //Customize chart options
+                  "hover": {
+                    "animationDuration": 0
+                  },
+                   "animation": {
+                      "duration": 1,
+                      "onComplete": function() {
+                        var chartInstance = this.chart,
+                        ctx = chartInstance.ctx;
+
+                        ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                        ctx.textAlign = 'center';
+                        ctx.textBaseline = 'bottom';
+
+                        this.data.datasets.forEach(function(dataset, i) {
+                          var meta = chartInstance.controller.getDatasetMeta(i);
+                          meta.data.forEach(function(bar, index) {
+                            var data = dataset.data[index];
+                            ctx.fillText(data, bar._model.x, bar._model.y - 5);
+                          });
+                        });
+                      }
+                  },
                   title: {
-                      display: true,
+                      display: false,
                       text: "",
                       fontColor: '#000000',
                   },
@@ -74,6 +96,9 @@ export default class Results extends Component {
                   scales: {
                       xAxes: [
                         {
+                        gridLines: {
+                          display: false,
+                        },
                         ticks: {
                               autoSkip: false,
                               maxRotation: 0,
@@ -85,9 +110,11 @@ export default class Results extends Component {
                       yAxes: [
                         {
                         gridLines: {
-                              display: true
+                              drawBorder: false,
+                              display: false,
                           },
                         ticks: {
+                              display: false,
                               fontColor: '#000000',
                               beginAtZero: true
                           }
